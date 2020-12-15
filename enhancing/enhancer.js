cb5872c153dffeb339ce55ff66e98d47a9e0f632
Alice Chang <sinsofangels@gmail.com>
2020-12-15 18:21:51 -0500

    Test items and enhancer functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,29 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  let newDurability = item.durability;
+  let newEnhancement = item.enhancement;
+  if (item.enhancement < 15) {
+    newDurability = item.durability - 5;
+  } else {
+    newDurability = item.durability - 10;
+  }
+  if (item.enhancement > 16) {
+    newEnhancement = item.enhancement - 1;
+  }
+  return { ...item, durability: newDurability, enhancement: newEnhancement };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
